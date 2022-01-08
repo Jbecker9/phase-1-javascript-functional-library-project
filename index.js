64da75b3515c25e1c6b499c540ba75997bf13253
Jared <jbeckersalax9@aol.com>
2022-01-07 22:03:02 -0500

    commit tree e94e2a98eae8dda5e60addfa5ef2b3a56d00c7f0

--- index.js
+++ index.js
@@ -46,3 +46,85 @@ function myReduce(collection, callback, acc){
     }
     return acc
 }
+
+function myFind(collection, predicate){
+    let findArray = []
+    if(typeof collection === 'object'){
+        findArray = Object.values(collection)
+    }
+    else{
+        findArray = [...collection]
+    }
+    for(let i = 0; i < findArray.length; i++){
+        if(predicate(findArray[i])){
+            return findArray[i]
+        }
+    }
+   
+}
+
+function myFilter(collection, predicate){
+    let filterArray = []
+    if(typeof collection === 'object'){
+        filterArray = Object.values(collection)
+    }
+    else{
+        filterArray = [...collection]
+    }
+    let trueArray = []
+    for(let i = 0; i < filterArray.length; i++){
+        if(predicate(filterArray[i])){
+        trueArray.push(filterArray[i])
+        }
+    }
+    return trueArray
+}
+
+function mySize(collection){
+    let sizeArray = []
+    if(typeof collection === 'object'){
+        sizeArray = Object.values(collection)
+    }
+    else{
+        sizeArray = [...collection]
+    }
+
+    let size = sizeArray.length
+    return size
+}
+
+function myFirst(array, integer){
+    let firstArray = []
+    if(integer){
+        for(let i = 0; i < integer; i++ ){
+            firstArray.push(array[i])
+        }
+        return firstArray
+    } else {
+        return array[0]
+    }
+}
+
+function myLast(array, integer){
+    let lastArray = []
+    if(integer){
+        for(let i = (array.length - integer); i < array.length; i++){
+            lastArray.push(array[i])
+        }
+        return lastArray
+    } else {
+        return array[array.length-1]
+    }
+}
+
+function myKeys(object){
+    let objArray = [] 
+    objArray = Object.keys(object)
+    return objArray
+}
+
+function myValues(object){
+    let valueArray = [] 
+    valueArray = Object.values(object)
+    return valueArray
+}
